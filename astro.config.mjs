d7dcbeb6c55fc73b0450c221f1824cd5d1d35137
Niekos Robbins <niekos.c.robbins@gmail.com>
2026-03-06 15:15:56 -0600

    fix: set site URL for sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://ncrdigital.com',
+  site: 'https://ncr-digital.vercel.app',
   integrations: [
     tailwind(),
     sitemap(),
