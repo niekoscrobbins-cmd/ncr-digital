fd06ec12ed30c9c56c7241539bb80cde1add2465
Niekos Robbins <niekos.c.robbins@gmail.com>
2026-03-06 16:08:40 -0600

    feat: new domain ncrdigi.com + redesigned Hero section

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://ncr-digital.vercel.app',
+  site: 'https://ncrdigi.com',
   trailingSlash: 'always',
   compressHTML: true,
   integrations: [
